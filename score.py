577ff0a730fb8482a3b7777a59015fc986abcd14
PicoJr <PicoJr@notanemail.com>
2019-03-02 13:00:06 +0100

    Add input file stats

--- score.py
+++ score.py
@@ -48,16 +48,22 @@ class Image(object):
         :param input_file_path: input file path
         :return: Image list
         """
+        verticals, horizontals = 0, 0
         logging.info("parsing %s", input_file_path)
         with open(input_file_path, 'r') as input_file:
-            _ = int(input_file.readline())
+            nb = int(input_file.readline())  # images nb
             images = []
             for i, img_txt in enumerate(input_file.readlines()):
                 data = img_txt.rstrip().split(' ')
                 orientation = data[0]
                 tags = data[2:]
                 images.append(Image(i, orientation, set(tags)))
+                if orientation == 'V':
+                    verticals += 1
+                else:  # H
+                    horizontals += 1
             logging.info('parsing %s done', input_file_path)
+            logging.info('%d images found (%d V,%d H)', nb, verticals, horizontals)
             return images
 
     @staticmethod
