731ce7659bacb1d147057fab9821cd4983c4ae72
PicoJr <PicoJr@notanemail.com>
2019-03-02 14:20:59 +0100

    Add check option and example invalid output files

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Hash Code 2019 Score Calculator
 
-**TL;DR** Compute score for Hash Code 2019 Qualification Round.
+**TL;DR** Compute score and check output for Hash Code 2019 Qualification Round.
 
 ## Usage
 
@@ -10,6 +10,80 @@
 score: 2
 ```
 
-## TODO
+### Check Output File
 
-- add `--check` option: check output file is valid
+use `--check` option.
+
+#### check images `id < maxid`
+
+```bash
+./score.py res/a_example.txt res/a_example_error_id_max.out --check
+```
+
+```
+INFO:root:parsing res/a_example.txt
+INFO:root:parsing res/a_example.txt done
+INFO:root:4 images found (2 V,2 H)
+INFO:root:parsing res/a_example_error_id_max.out
+ERROR:root:image id: 4 > max id: 3, at line: 2
+INFO:root:parsing res/a_example_error_id_max.out: done
+ERROR:root:invalid output file: res/a_example_error_id_max.out
+score : 0
+```
+
+#### check images `id` occur at most once
+
+```bash
+./score.py res/a_example.txt res/a_example_error_same_id_twice.out --check
+```
+
+```
+INFO:root:parsing res/a_example.txt
+INFO:root:parsing res/a_example.txt done
+INFO:root:4 images found (2 V,2 H)
+INFO:root:parsing res/a_example_error_same_id_twice.out
+ERROR:root:image id: 3 found again at line: 4
+INFO:root:parsing res/a_example_error_same_id_twice.out: done
+ERROR:root:invalid output file: res/a_example_error_same_id_twice.out
+score : 0
+```
+
+#### check images `orientation` is valid
+
+```bash
+./score.py res/a_example.txt res/a_example_error_orientation.out --check
+```
+
+```
+INFO:root:parsing res/a_example.txt
+INFO:root:parsing res/a_example.txt done
+INFO:root:4 images found (2 V,2 H)
+INFO:root:parsing res/a_example_error_orientation.out
+ERROR:root:image id: 0 (V expected: H) at line: 1
+ERROR:root:image id: 3 (V expected: H) at line: 1
+INFO:root:parsing res/a_example_error_orientation.out: done
+ERROR:root:invalid output file: res/a_example_error_orientation.out
+score : 0
+```
+
+#### Check And Abort On 1st Error
+
+`--check` reports all errors.
+
+use `--abort` option to abort on 1st error.
+
+```
+./score.py res/a_example.txt res/a_example_error_all.out --check --abort
+```
+
+```bash
+INFO:root:parsing res/a_example.txt
+INFO:root:parsing res/a_example.txt done
+INFO:root:4 images found (2 V,2 H)
+INFO:root:parsing res/a_example_error_all.out
+ERROR:root:image id: 3 found again at line: 4
+INFO:root:aborting...
+INFO:root:parsing res/a_example_error_all.out: done
+ERROR:root:invalid output file: res/a_example_error_all.out
+score : 0
+```
--- score.py
+++ score.py
@@ -5,6 +5,10 @@ import logging
 from timeit import default_timer as timer
 
 
+class InvalidOutputFileException(Exception):
+    pass
+
+
 class Image(object):
     def __init__(self, image_id, orientation, tags):
         self.image_id = image_id
@@ -71,13 +75,13 @@ class Image(object):
         """
         Parse output file
         :param output_file_path: output file path (solution)
-        :return: Image list ((Image, None)|(Image, Image)) list
+        :return: ((Image, None)|(Image, Image)) list
         """
         logging.info("parsing %s", output_file_path)
         slides = []
         with open(output_file_path, 'r') as output_file:
             _ = output_file.readline()  # slide show size
-            for line in output_file.readlines():
+            for output_line, line in enumerate(output_file.readlines()):
                 image_id_tuple = tuple((int(image_id) for image_id in line.rstrip().split(' ')))
                 if len(image_id_tuple) == 2:
                     img0 = Image(image_id_tuple[0], "V", [])
@@ -88,6 +92,67 @@ class Image(object):
                     slides.append((img, None))
         logging.info("parsing %s: done", output_file_path)
         return slides
+
+    @staticmethod
+    def parse_output_and_check(output_file_path, input_images, abort=False):
+        """
+        Parse output file
+        :param input_images: Image list from input file
+        :param output_file_path: output file path (solution)
+        :return: ((Image, None)|(Image, Image)) list
+        """
+        logging.info("parsing %s", output_file_path)
+        slides = []
+        image_id_set = set()
+        valid = True
+        with open(output_file_path, 'r') as output_file:
+            _ = output_file.readline()  # slide show size
+            for output_line, line in enumerate(output_file.readlines()):
+                image_id_tuple = tuple((int(image_id) for image_id in line.rstrip().split(' ')))
+                if len(image_id_tuple) == 2:
+                    img0 = Image(image_id_tuple[0], "V", [])
+                    valid = Image.check_image(img0, input_images, image_id_set,
+                                              output_line + 1) and valid  # in that order to avoid lazy evaluation
+                    image_id_set.add(img0.image_id)
+                    img1 = Image(image_id_tuple[1], "V", [])
+                    valid = Image.check_image(img1, input_images, image_id_set,
+                                              output_line + 1) and valid  # in that order to avoid lazy evaluation
+                    image_id_set.add(img1.image_id)
+                    slides.append((img0, img1))
+                else:
+                    img = Image(image_id_tuple[0], "H", [])
+                    valid = Image.check_image(img, input_images, image_id_set,
+                                              output_line + 1) and valid  # in that order to avoid lazy evaluation
+                    image_id_set.add(img.image_id)
+                    slides.append((img, None))
+                if abort and not valid:
+                    logging.info("aborting...")
+                    break
+        logging.info("parsing %s: done", output_file_path)
+        if not valid:
+            raise InvalidOutputFileException("invalid output file: {}".format(output_file_path))
+        else:
+            logging.info("%s is valid", output_file_path)
+        return slides
+
+    @staticmethod
+    def check_image(image, input_images, image_id_set, output_line):
+        max_id = len(input_images) - 1
+        valid = True
+        if image.image_id > max_id:
+            logging.error("image id: %d > max id: %d, at line: %d", image.image_id, max_id, output_line)
+            valid = False
+        elif image.image_id in image_id_set:
+            logging.error("image id: %d found again at line: %d", image.image_id, output_line)
+            valid = False
+
+        if image.image_id <= max_id:
+            input_image = input_images[image.image_id]
+            if image.orientation != input_image.orientation:
+                logging.error("image id: %d (%s expected: %s) at line: %d", image.image_id, image.orientation,
+                              input_image.orientation, output_line)
+                valid = False
+        return valid
 
     @staticmethod
     def compute_score_slides(slides, images):
@@ -117,6 +182,8 @@ def main():
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('input_file_path', type=str, help='input file path e.g. a_example.in')
     parser.add_argument('output_file_path', type=str, help='output file path e.g. a_example.out')
+    parser.add_argument('--check', action='store_true', help='check output file')
+    parser.add_argument('--abort', action='store_true', help='use with --check, abort on first error')
     parser.add_argument('--debug', action='store_true', help='for debug purpose')
     args = parser.parse_args()
     set_log_level(args)
@@ -128,7 +195,14 @@ def main():
     logging.debug('parsing input took %s s', end - start)
 
     start = timer()
-    slides = Image.parse_output(args.output_file_path)
+    slides = []
+    if args.check:
+        try:
+            slides = Image.parse_output_and_check(args.output_file_path, images, args.abort)
+        except InvalidOutputFileException as ex:
+            logging.error(ex)
+    else:
+        slides = Image.parse_output(args.output_file_path)
     end = timer()
     logging.debug('parsing output took %s s', end - start)
 
